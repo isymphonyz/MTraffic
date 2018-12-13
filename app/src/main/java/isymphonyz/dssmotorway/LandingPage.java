f288dd1ec8e5065f2ac23290b714a3135e4cf285
chatchai <chatchai.keng@gmail.com>
2018-12-13 20:38:19 +0700

    Initial commit

---
+++ app/src/main/java/isymphonyz/dssmotorway/LandingPage.java
@@ -0,0 +1,13 @@
+package isymphonyz.dssmotorway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+    }
+}
